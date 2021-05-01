8c2b66b978b4953fca86c15187004e972e286ea9
roblebob <robert.koppisch@gmail.com>
2021-05-01 23:05:17 +0200

    ...

--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -3,13 +3,24 @@ struct Point1Typed<T> { x: T, y: T, }
 struct Point2Typed<T,U> { x: T, y: U, }
 
 impl<T> Point1Typed<T> {
-    fn x(&self) -> &T { &self.x }
-    fn y(&self) -> &T { &self.y }
+    fn x(&self) -> &T { 
+        &self.x 
+    }
+    fn y(&self) -> &T { 
+        &self.y 
+    }
+}
+
+
+impl Point1Typed<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
 }
 
 
 fn main() {
     let p = Point1Typed { x: 5, y: 10 };
 
-    println!("p.x = {}", p.x());
+    println!("p.x = {} and p.y = {}", p.x(), p.y());
 }
