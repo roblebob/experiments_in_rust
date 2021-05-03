6c15480b980fe61121ddea4714736b9416d39954
roblebob <robert.koppisch@gmail.com>
2021-05-03 22:53:35 +0200

    ...

--- generic_types/src/main.rs
+++ generic_types/src/main.rs
@@ -1,7 +1,5 @@
 struct Point1Typed<T> { x: T, y: T, }
 
-struct Point2Typed<T,U> { x: T, y: U, }
-
 impl<T> Point1Typed<T> {
     fn x(&self) -> &T { 
         &self.x 
@@ -11,7 +9,6 @@ impl<T> Point1Typed<T> {
     }
 }
 
-
 impl Point1Typed<f32> {
     fn distance_from_origin(&self) -> f32 {
         (self.x.powi(2) + self.y.powi(2)).sqrt()
@@ -19,8 +16,29 @@ impl Point1Typed<f32> {
 }
 
 
+
+struct Point2Typed<T,U> { x: T, y: U, }
+
+impl<T, U> Point2Typed<T, U> {
+    fn mixup<V, W>(self, other: Point2Typed<V, W> ) -> Point2Typed<T, W> {
+        Point2Typed{
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+
+
+
 fn main() {
     let p = Point1Typed { x: 5, y: 10 };
-
     println!("p.x = {} and p.y = {}", p.x(), p.y());
+
+    
+    let p1 = Point2Typed { x: 5, y: 10.4 };
+    let p2 = Point2Typed { x: "Hello", y: 'c' };
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}  p3.y = {}", p3.x, p3.y);
 }
