749282d869a757e0c5a98ecdb003e382e0bfe7bf
roblebob <robert.koppisch@gmail.com>
2021-05-27 12:16:37 +0200

    some progress within the lifetime chapter

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,15 +1,3 @@
-fn main() {
-    
-    let string1 = String::from("abcd");
-    let result;
-    {
-        let string2 = String::from("xyz");
-
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is {}", result);
-}
-
 
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
@@ -19,4 +7,36 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     } else {
         y
     }
+}
+
+
+fn main() {
+    
+    let string1 = String::from("abcdefg");
+    let result;
+    let string2: String;
+    {
+        string2 = String::from("xyz");
+
+        result = longest(string1.as_str(), string2.as_str());
+    }
+    println!("The longest string is {}", result);
+
+
+
+
+    let novel = String::from("Call me Ishmael. Some years ago....");
+    let first_sentence = novel.split('.').next().expect("Coul not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("{}", i.part);
+}
+
+
+
+
+struct ImportantExcerpt<'a> {
+
+    part: &'a str,
 }
