4807816598e02adcecfac4f2f9f61ca904745752
roblebob <robert.koppisch@gmail.com>
2021-07-13 17:41:31 +0200

    functional: extending cacher: first TDD cycle completed

--- functional/src/cacher.rs
+++ functional/src/cacher.rs
@@ -1,31 +1,47 @@
-pub struct Cacher<T>
+enum Status<T> {
+    Cached(T),
+    Computed(T),
+}
+
+
+
+pub struct Cacher<F>
 where
-    T: Fn(u32) -> u32
+    F: Fn(u32) -> u32
 {
-    calculation: T,
+    argument: Option<u32>,
+    calculation: F,
     value: Option<u32>,
 }
 
-impl<T> Cacher<T>
+impl<F> Cacher<F>
 where
-    T: Fn(u32) -> u32
+    F: Fn(u32) -> u32
 {
-    pub fn new(calculation: T) -> Cacher<T> {
+    pub fn new(calculation: F) -> Cacher<F> {
         Cacher {
+            argument: None,
             calculation,
             value: None,
         }
     }
 
     pub fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
+
+        match self.argument {
+            Some(x) => {
+                if x==arg {self.value.unwrap()} 
+                else {self.helper(arg)}
+              },
+            None => self.helper(arg),
         }
+    }
+
+    fn helper(&mut self, arg: u32) -> u32 {
+        self.argument = Some(arg);        
+        let v = (self.calculation)(arg);
+        self.value = Some(v);
+        v    
     }
 }
 
@@ -36,11 +52,11 @@ mod tests {
 
     #[test]
     fn call_with_different_values() {
-        let mut c = Cacher::new(|a| a);
+        let mut c = Cacher::new(|a| a*10);
 
         let v1 = c.value(1);
         let v2 = c.value(2);
 
-        assert_eq!(v2, 2);
+        assert_eq!(v2, 20);
     }
 }
