d1d01e4dc7bd1bc2dcf131ddfbdddd7508b9537e
roblebob <robert.koppisch@gmail.com>
2021-07-20 15:17:39 +0200

    functional: extended cacher: refactored so argument passed in by value is a reference

--- functional/src/cacher.rs
+++ functional/src/cacher.rs
@@ -39,18 +39,19 @@ where
         }
     }
 
-    pub fn value(&mut self, arg: T) -> T {
+    pub fn value(&mut self, arg: &T) -> T {
         self.value_with_status(arg).unwrap()
     }
 
-    pub fn value_with_status(&mut self, arg: T) -> Status<T> {
+    pub fn value_with_status(&mut self, arg: &T) -> Status<T> {
 
-        let key: String = format!("{:?}", arg);
+        let key: String = format!("{:?}", *arg);
 
         if self.cache_map.contains_key(&key) {
             Status::Cached(self.cache_map[&key].clone())
         } else {
-            let v = (self.calculation)(arg);
+            let a = (*arg).clone();
+            let v = (self.calculation)(a);
             self.cache_map.insert(key, v.clone());
             Status::Computed(v)
         }
@@ -66,8 +67,8 @@ mod tests {
     fn call_with_different_values() {
         let mut c = Cacher::new(|a| a*10);
 
-        let v1 = c.value(1);
-        let v2 = c.value(2);
+        let v1 = c.value(&1);
+        let v2 = c.value(&2);
 
         assert_eq!(v2, 20);
     }
@@ -78,22 +79,22 @@ mod tests {
 
         let mut  ar = -1;
 
-        let v1 = c.value(ar);
+        let v1 = c.value(&ar);
         
         ar = -2;
-        let v2 = c.value_with_status(ar);
+        let v2 = c.value_with_status(&ar);
         match v2 {
             Status::Cached(_) => panic!("Should not have happened! Was called the first time!"),
             Status::Computed(x) => assert_eq!(x, -20),
         }
-        let v2 = c.value_with_status(ar);
+        let v2 = c.value_with_status(&ar);
         match v2 {
             Status::Cached(x) => assert_eq!(x, -20),
             Status::Computed(_) => panic!("Should not have happend! Proofs cache's memory is less than 1!"),
         }
 
         ar = -1;
-        let v1 = c.value_with_status(ar);
+        let v1 = c.value_with_status(&ar);
         match v1 {
             Status::Cached(x) => assert_eq!(x, -10),
             Status::Computed(_) => panic!("TDD failing test for introducing cache memory larger than 1!"),
@@ -103,7 +104,7 @@ mod tests {
     #[test]
     fn call_with_floats() {
         let mut c = Cacher::new(|a| a);
-        let v = c.value(1.1);
+        let v = c.value(&1.1);
         assert_eq!(v, 1.1);
     }
 
@@ -111,7 +112,7 @@ mod tests {
     fn call_with_strings() {
         let mut c = Cacher::new(|a: String| a.clone());
         let ar = "Hallo".to_string();
-        let v = c.value(ar.clone());
+        let v = c.value(&ar);
         assert_eq!(v, ar);
     }
 }
