420389caf982530b70dc4e0c585a4da21b168663
roblebob <robert.koppisch@gmail.com>
2021-09-06 10:40:54 +0200

    finished ch16.3.

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -125,4 +125,51 @@ fn main() {
     for received in rx {
         println!("Got: {}", received);
     }
+
+
+
+
+    println!("=========================================================");
+
+    {
+        use std::sync::Mutex;
+
+        let m = Mutex::new(5);
+
+        {
+            let mut num = m.lock().unwrap();
+            *num = 6;
+        } // num goes out of scope and thereby unlocks 
+
+        println!("m = {:?}", m);
+    }
+
+
+    println!("--------------------------------------------");
+
+    {
+        use std::sync::{Mutex, Arc};
+        use std::thread;
+
+
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
+        
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+
+            let handle = thread::spawn( move || {
+               let mut num = counter.lock().unwrap();
+
+               *num += 1;
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        println!("Result: {}", *counter.lock().unwrap());
+    }
 }
