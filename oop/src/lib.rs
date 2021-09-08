5d98da8425f4c89387aee949915c5706a314a99a
roblebob <robert.koppisch@gmail.com>
2021-09-08 15:47:32 +0200

    started ch17 OOP

---
+++ oop/src/lib.rs
@@ -0,0 +1,42 @@
+pub struct AveragedCollection {
+    list: Ves<i32>,
+    average: f64,
+}
+
+
+impl AverageCollection {
+    pub fn add(&mut self, value:i32) {
+        self.list.push(value);
+        self.uodate_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(avlue)
+            }
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
