5420d3ca6103b5e6dfc7fda6289a2d274d70118b
roblebob <robert.koppisch@gmail.com>
2021-09-22 09:27:15 +0200

    finished half of 18.3

--- patterns_and_matching/src/main.rs
+++ patterns_and_matching/src/main.rs
@@ -50,6 +50,168 @@ fn main() {
     }
 
     {
-        let Some(x) = some_option_value;
+        let x = 5;
+
+        match x {
+            1 | 2 => println!("one or two"),
+            3 => println!("three"),
+            4..=6 => println!("four through six"),  
+            _ => println!("anything"),
+        }
+    }
+
+    {
+        let x = Some(5);
+        let y = 10;
+
+        match x {
+            Some(50) => println!("Got 50"),
+            Some(y) => println!("Matched, y = {:?}", y),
+            _ => println!("Default case, x = {:?}", x),
+        }
+
+        println!("at the end: x = {:?}, y = {:?}", x, y);
+    }
+
+
+    {
+        let x = 'c';
+
+        match x {
+            'a'..='j' => println!("early ASCII letter"),
+            'k'..='z' => println!("late ASCII letter"),
+            _ => println!("Something else"),
+        }
+    }
+
+
+    // Destructing Structs
+
+    {
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        let p = Point { x: 0, y: 7 };
+
+
+        {
+            let Point { x: a, y: b} = p;
+            assert_eq!(0, a);
+            assert_eq!(7, b);
+        }
+        {
+            let Point { x, y } = p;
+            assert_eq!(0, x);
+            assert_eq!(7, y);
+        }
+        {
+            match p {
+                Point { x, y: 0 } => println!("On the x axis at {}", x),
+                Point { x: 0, y } => println!("On the y axis at {}", y),
+                Point { x, y }    => println!("On neither axis: ({}, {})", x, y),
+            }
+        }
+    }
+
+    // Destructing Enums
+   
+    {
+        enum Message {
+            Quit,
+            Move { x: i32, y: i32 },
+            Write(String),
+            ChangeColor(i32, i32, i32),
+        }
+
+        let msg = Message::ChangeColor(0, 160, 255);
+
+        match msg {
+            Message::Quit          => { println!("The Quit variant has no data to destructure.") }
+            Message::Move { x, y } => { println!("Move in the x direction {} and in the y direction {}", x, y); } 
+            Message::Write(text)   =>   println!("Text message: {}", text),
+            Message::ChangeColor(r, g, b) => println!("Change the color to red {}, green {}, and blue {}", r, g, b),
+        }
+    }
+
+    // Destructing Nested Structs and Enums 
+
+    {
+        enum Color {
+            Rgb(i32, i32, i32),
+            Hsv(i32, i32, i32),
+        }
+
+        enum Message {
+            Quit,
+            Move { x: i32, y: i32 },
+            Write(String),
+            ChangeColor(Color),
+        }
+
+        let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+        match msg {
+            Message::ChangeColor(Color::Rgb(r, g, b)) => println!("Change the color to red {}, green {}, and blue {}", r, g, b),
+            Message::ChangeColor(Color::Hsv(h, s, v)) => println!("Change the color to hue {}, saturation {}, and value {}", h, s, v),
+            _ => (),
+        }
+    }
+
+    // Destructuring Structs and Tuples
+    {
+        struct Point { x: i32, y: i32 }
+
+        let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10});
+    }
+
+
+
+
+
+
+
+
+    // Ignoring Values in a Pattern
+    
+    {
+        fn foo(_: i32, y: i32) { println!("This code only uses the y parameter: {}", y); }
+
+        foo(3, 4);
+
+        // useful when using a general signature of some trait, but its particular implementation
+        // does not make use it -> not warn about unused parameters
+    }
+
+
+    // Ignoring Parts of a Value with a Nested _
+    {
+        let mut setting_value = Some(5);
+        let new_setting_value = Some(10);
+
+        match (setting_value, new_setting_value) {
+            (Some(_), Some(_)) => { println!("Can't overwrite an existing customized value"); }
+            _ => { setting_value = new_setting_value; }
+        }
+    }
+    {
+        let numbers = (2, 4, 8, 16, 32);
+
+        match numbers {
+            (first, _, third, _, fifth) => { println!("Some numbers: {}, {}, {}", first, third, fifth); }
+        }
+    }
+
+    // Ignoring an Unused Variable by Starting Its Name with _, to avoid warning of unused 
+    {
+        let s = Some(String::from("Hello!"));
+
+        // if let Some(_s) = s { println!("found a string"); }
+        // ... would produce an error, because s is moved into _s and therefore cannot be used afterwards 
+        if let Some(_) = s { println!("found a string"); }
+        // ... works, because it never binds s to anything
+
+        println!("{:?}", s);
     }
 }
