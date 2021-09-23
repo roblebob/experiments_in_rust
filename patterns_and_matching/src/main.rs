2b4a6bf6e54f5bcd6c90dabb16e392cf673c2bc9
roblebob <robert.koppisch@gmail.com>
2021-09-23 14:08:24 +0200

    finished chapter 18

--- patterns_and_matching/src/main.rs
+++ patterns_and_matching/src/main.rs
@@ -214,4 +214,82 @@ fn main() {
 
         println!("{:?}", s);
     }
+
+    // Ignoring Remaining Parts of a Value with ..
+    {
+        struct Point {
+            x: i32,
+            y: i32,
+            z: i32,
+        }
+
+        let origin = Point { x: 0, y: 0, z: 0 };
+
+        match origin {
+            Point { x, .. } => println!("x is {}", x),
+        }
+    }
+    {
+        let numbers = (2, 4, 8, 16, 32);
+
+        match numbers {
+            (first, .., last) => {
+                println!("Some numbers: {}, {}", first, last);
+            }
+        }
+    }
+
+    // Extra Conditionals with Match Guards
+    
+    {//18-26
+        let num = Some(4);
+
+        match num {
+            Some(x)  if x < 5  => println!("less than five: {}", x),
+            Some(x)            => println!("{}", x),
+            None               => (),
+        }
+    }
+    {//18-27
+        let x = Some(5);
+        let y = 10;
+
+        match x {
+            Some(50) => println!("Got 50"),
+            Some(n)  if n == y => println!("Matched, n = {}", n),
+            _ => println!("Default case, x = {:?}", x),
+        }
+
+        println!("at the end: x = {:?}, y = {}", x, y);
+    }
+    {//18-28
+        let x = 4;
+        let y = false;
+
+        match x {
+            4 | 5 | 6  if y  =>  println!("yes"),
+            _ => println!("no"),
+        }
+    }
+     
+
+    // @ Bindings
+
+    {
+        enum Message {
+            Hello { id: i32 },
+        }
+
+        let msg = Message::Hello { id: 5 };
+
+        match msg {
+            Message::Hello {
+                id: id_variable @ 3..=7,
+            } => println!("Found an id in range: {}", id_variable),
+            Message::Hello { id: 10..=12 } => {
+                println!("Found an id in another range")
+            }
+            Message::Hello { id } => println!("Found some other id: {}", id), 
+        }
+    }
 }
